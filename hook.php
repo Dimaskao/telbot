6e0cb614da5dd6d81f9d5ba8f35a011ea77153af
Dmytro Lytvynchuk <dmytrolutv@gmail.com>
2021-03-16 19:56:12 +0200

    wip

--- hook.php
+++ hook.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
-include "secret.php";//TODO: Возможно изменить на .env
+require "secret.php";//TODO: Возможно изменить на .env
 
 
 $API_KEY = $token; 
