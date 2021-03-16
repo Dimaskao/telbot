806ad5d9ba8e352e4f91318cce93fb611c147d80
Dmytro Lytvynchuk <dmytrolutv@gmail.com>
2021-03-16 17:54:31 +0200

    Initial commit v2.0

---
+++ commands/HelpCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Request;
+
+class TestCommand extends UserCommand
+{
+    protected $name = 'help';                      
+    protected $description = 'Команда для отображения списка команд'; 
+    protected $usage = '/help';                    
+    protected $version = '1.0.0';                  
+
+    public function execute(): ServerResponse
+    {
+        $message = $this->getMessage();
+
+        $chat_id = $message->getChat()->getId();
+
+        $data = [
+            'chat_id' => $chat_id,
+            'text'    => '/help - получить список команд',
+        ];
+
+        return Request::sendMessage($data);
+    }
+}
---
+++ hook.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+
+$API_KEY = '----<TOKEN>----'; //TODO: Добавить .env
+$USER_ID = 406235431;
+$BOT_NAME = "Learn english words";
+$mysql_credentials = [
+   'host'     => 'localhost',
+   'user'     => 'root',
+   'password' => 'root',
+   'database' => 'telbot',
+];
+
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\TelegramLog;
+
+try {
+	$telegram = new Telegram($API_KEY, $BOT_NAME);
+	//$telegram->enableMySQL($mysql_credentials);
+
+	$telegram->addCommandsPath(__DIR__ . "/commands");
+	$telegram->enableAdmin((int)$USER_ID);
+
+	TelegramLog::initUpdateLog($BOT_NAME . '_update.log');
+
+	$telegram->handle();
+
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+	var_dump($e);
+}
