864cf3dd8e43561fb449474fd40ad946e702ba17
Dmytro Lytvynchuk <dmytrolutv@gmail.com>
2021-03-16 21:19:44 +0200

    wip

--- commands/AddCommand.php
+++ commands/AddCommand.php
@@ -6,7 +6,7 @@ use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Request;
 
-class TestCommand extends UserCommand
+class AddCommand extends UserCommand
 {
     protected $name = 'add';
     protected $description = 'Добавления новых слов в словарь изучения';
