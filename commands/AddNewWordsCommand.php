9b8235cead795ab8c5f01404be428995b0ea1fa6
Dmytro Lytvynchuk <dmytrolutv@gmail.com>
2021-03-16 20:09:06 +0200

    Added AddNweWordsCommand.php

---
+++ commands/AddNewWordsCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Request;
+
+class HelpCommand extends UserCommand
+{
+    protected $name = 'add';
+    protected $description = 'Добавления новых слов в словарь изучения';
+    protected $usage = '/add';
+    protected $version = '1.0.0';
+
+    public function execute(): ServerResponse
+    {
+        $message = $this->getMessage();
+        $chat_id = $message->getChat()->getId();
+
+        $text = $message->getText();
+        $data = [
+            'chat_id' => $chat_id,
+            'text'    => $text,
+        ];
+
+        return Request::sendMessage($data);
+    }
+}
